07888fa27983f683f637bdcf47d89ea90e1ddcb8
ALP300 <aitorpalomino1@gmail.com>
2026-03-11 22:21:03 -0500

    a

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://proy-back-dnivel-44j5.onrender.com/api'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://proy-back-dnivel-44j5.onrender.com/api'
+};
